27709e4664fad6080bcaca7766bd9eb9a48ca9d8
Seedahant Kalra <seedhantkalra@gmail.com>
2025-03-27 15:28:52 -0400

    env

--- src/config.js
+++ src/config.js
@@ -6,3 +6,8 @@ dotenv.config();
 export const config = {
   port: process.env.PORT || 5000,
 };
+
+export const sessionSecret = process.env.SESSION_SECRET;
+export const openaiApiKey = process.env.OPENAI_API_KEY;
+export const mongoUri = process.env.MONGO_URI;
+export const encryptionSecret = process.env.ENCRYPTION_SECRET;
