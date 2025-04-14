28720591ba5a6d76472feaac9bf5fec22a2c13d6
Seedahant Kalra <seedhantkalra@gmail.com>
2025-04-14 10:10:50 -0400

    frontend unneccesary comments removed

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div className="chat-wrapper"> {/* ✅ Wrap here */}
+    <div className="chat-wrapper"> {/* Wrap here */}
       <App />
     </div>
   </React.StrictMode>
